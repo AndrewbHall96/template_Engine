2e9d232a70be707423be6e5a893fd70caf02d892
Andrew Hall <andrewbhall96@gmail.com>
2020-07-02 16:55:58 -0400

    updating getId getName getRole function

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -5,10 +5,27 @@ class Employee {
         this.name = name;
         this.id = id;
         this.email = email;
+        this.role = "Employee";
     }
 
     sayName() {
         return 'Hello! My name is ' + this.name + '! ';
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
     }
 }
 
